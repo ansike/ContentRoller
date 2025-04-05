5349a3f910134d0c6d6ae97194184cb7f7a0cee0
cisco.a <cisco.a@local.com>
2025-04-05 11:49:04 +0800

    feat: init file

---
+++ example/index.tsx
@@ -0,0 +1,19 @@
+import 'react-app-polyfill/ie11';
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Thing } from '../.';
+
+const App = () => {
+  return (
+    <div>
+      <Thing />
+    </div>
+  );
+};
+
+
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+}
